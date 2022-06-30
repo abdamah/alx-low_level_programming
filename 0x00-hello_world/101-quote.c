e0f24f6dffcdb7b96095e3329d262fd4cd61fc91
Abdallah <abdamoh1@gmail.com>
2022-06-30 07:55:53 -0700

    added: 101-quote.c file , script that prints text without using printf and puts funtions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 {
 char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-write(2, quote, sizeof(quote));
+write(2, quote, sizeof(quote) - 1);
 
 return (1);
 }
