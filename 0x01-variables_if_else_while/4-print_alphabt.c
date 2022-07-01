9a1811d6a2cdb2afe8b5a030ce9973bf49201520
Abdallah <abdamoh1@gmail.com>
2022-07-01 06:45:03 -0700

    added: 4-print_alphabt.c file , program that prints lower alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
