656702ba33f6a2d644fb83ab1baf60a882763dbc
Abdallah <abdamoh1@gmail.com>
2022-07-01 07:12:39 -0700

    added: 7-print_tabahpla.c file, program print lower alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tabahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c -= 1;
+	}
+	putchar('\n');
+
+	return (0);
+}
