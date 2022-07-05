fd0fd7a3fbf7b919655bb6b5773691646444cdfa
Abdallah <abdamoh1@gmail.com>
2022-07-05 08:05:10 -0700

    Fix: main.h header added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - entry point
- *
- * Return: Always 0 (success)
- */
+*main - prints "_putchar"
+*
+*Return: Alwways 0 (success".
+*/
+
 int main(void)
 {
-	printf("_putchar\n");
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
 
 	return (0);
 }
