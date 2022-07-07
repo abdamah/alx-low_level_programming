3a4b7b73ded163265de8e0d23926753b31398e80
Abdallah <abdamoh1@gmail.com>
2022-07-07 09:02:32 -0700

    Fix: 101-print_number.c file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -24,37 +24,3 @@ void print_number(int n)
 		num_to_char(un);
 	}
 }
-
-/**
- * number_to_char - converts a number with 1 or more digits into a char
- * @n: number to print
- *
- * Return: void
- */
-void number_to_char(unsigned int n)
-{
-	unsigned int d = 10;
-
-	if (n < d)
-	{
-		_putchar('0' + n);
-	}
-	else
-	{
-		while (n >= d)
-		{
-			d *= 10;
-			if (d == 1000000000)
-				break;
-		}
-		if (!(d == 1000000000) || n == 123456789)
-			d /= 10;
-		_putchar('0' + n / d);
-		while (d != 10)
-		{
-			d /= 10;
-			_putchar('0' + (n / d) % 10);
-		}
-		_putchar('0' + n % 10);
-	}
-}
