3df2beef4362c343478ee054a8fe240c0c580581
Abdallah <abdamoh1@gmail.com>
2022-07-11 09:34:46 -0700

    added: 4-print_rev.c file, prints a given string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a given string in reverse order
+ * @s: pionter parameter
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+
+	while (count > 0)
+	{
+		s--;
+		_putchar(*s);
+		count--;
+	}
+	_putchar('\n');
+}
