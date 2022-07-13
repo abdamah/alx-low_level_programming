83bf8e5348cb8e74891316bc035ecb9c1737c27d
Abdallah <abdamoh1@gmail.com>
2022-07-13 13:01:28 -0700

    added: 7-leet.c file, encodes a given string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* leet - encode a given string
+*@str: source parameter
+*Return: char *
+*/
+
+char *leet(char *str)
+{
+	int i = 0, j;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[++i])
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			if (str[i] == leet[j] ||
+			 str[i] - 32 == leet[j])
+				str[i] = j + '0';
+		}
+	}
+	return (str);
+}
