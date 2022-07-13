a062622911e0c0c2e75903f0ce3e84ccb15cf05d
Abdallah <abdamoh1@gmail.com>
2022-07-13 12:37:30 -0700

    added: 5-string_toupper.c file, to make uppercase letters

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*string_toupper - converts a given string to upper case.
+*@str: parameter
+*Return: char *
+*/
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+
+	return (str);
+}
