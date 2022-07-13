b257b14996e4e07c4a670728ff8665bd7e9f9802
Abdallah <abdamoh1@gmail.com>
2022-07-13 13:43:26 -0700

    fix: 101-print_number.c file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
-* print_number - prints an integer numbers
+* print_number - prints an integer
 *@n:integer to be printed
 *
 */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int number = n;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		num = -num;
+		_putchar(45); /**ASCII code '-; is '45'*/
+		number = -number;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	if ((number / 10) > 0)
+		print_number(number / 10);
 
-	_putchar((num % 10) + '0');
+	_putchar((number % 10) + '0');
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@
  */
 void reverse_array(int *a, int n);
 void print_number(int n);
+int _putchar(char c);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
