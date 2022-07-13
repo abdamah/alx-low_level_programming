d01d33c7f3d06e26616d83d1bc64232088ef1440
Abdallah <abdamoh1@gmail.com>
2022-07-13 11:30:23 -0700

    added: 1-strncat.c file , to cancatenate two given strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strncat - concatenate two given strings
+ *@dest: destination parameter
+ *@src: source parameter
+ *@n:integer number of bytes 
+ *Return: char *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+
+}
