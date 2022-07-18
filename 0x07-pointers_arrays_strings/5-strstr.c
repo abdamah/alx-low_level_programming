a9eb04c54bc591a83c26a26fa39deae3d984369a
Abdallah <abdamoh1@gmail.com>
2022-07-18 11:00:26 -0700

    added: 5-strstr.c file, locates substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+*_strstr - ocates a substring.
+*@haystack: stack parameter
+*@needle: needle parameter
+*Return: char * || NULL
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
