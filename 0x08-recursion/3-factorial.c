20a6fa05157630ca7d75790ed590c297a303d017
Abdallah <abdamoh1@gmail.com>
2022-07-20 07:43:25 -0700

    added: 3-factorial.c file, finds factorial of a given nubmer

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* factorial - find factorial of a given number
+* @n: the number to be found
+*
+* Return: int
+*/
+
+int factorial(int n)
+{
+	int next;
+
+	if (n < 0)
+		return (-1);
+	else if  (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
