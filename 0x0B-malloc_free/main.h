e6de4570e1a598883773f89d4a45d05a00db1fef
Abdallah <abdamoh1@gmail.com>
2022-07-25 11:35:14 -0700

    added: 101-strtow.c file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,8 +8,8 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 char *argstostr(int ac, char **av);
-char **strtow(char *str);
-int **alloc_grid(int width, int height);
 int length(char *str);
-int num_words(char *str);
+char **strtow(char *str);
+int word_count(char *s);
+int **alloc_grid(int width, int height);
 #endif /* MAIN_H */
