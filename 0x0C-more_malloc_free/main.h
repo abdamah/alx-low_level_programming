4f97b518e2ccb43a4fc8c5f2632dae151eae1111
Abdallah <abdamoh1@gmail.com>
2022-07-28 08:01:56 -0700

    fix: 101-mul.c file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,13 +9,12 @@
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-int len(char *str);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int find_len(char *str);
+int len(char *str);
 char *create_xarray(int size);
-char *iterate_zeroes(char *str);
+int get_digit(char c);
 void get_prod(char *prod, char *mult, int digit, int zeroes);
 void add_nums(char *final_prod, char *next_prod, int next_len);
 
