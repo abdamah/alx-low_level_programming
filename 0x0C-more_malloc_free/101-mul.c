b83a8ffe10c106fed857126157ae80ab3af14ca6
Abdallah <abdamoh1@gmail.com>
2022-07-28 08:11:29 -0700

    fix: 101-mul.c file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+int len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
 /**
  * len - Finds the length of a string.
  * @str: The string to be measured.
