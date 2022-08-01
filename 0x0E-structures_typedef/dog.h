28e20c6afaadf04a18014a77a895db8623b6420f
Abdallah <abdamoh1@gmail.com>
2022-08-01 06:06:26 -0700

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,18 @@ float age;
 char *owner;
 };
 
+
+/**
+*dog_t - Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+
 #endif
