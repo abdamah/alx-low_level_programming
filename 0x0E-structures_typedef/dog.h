fcfa85aa2e1b5786c8be3d8b51ac25789d77c8b9
Abdallah <abdamoh1@gmail.com>
2022-08-01 05:51:14 -0700

    Define: new type dod.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*struct dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
