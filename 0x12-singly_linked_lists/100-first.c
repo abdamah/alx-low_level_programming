08a50be6fa1a8f477b5d5f7530b916938cc115a1
Abdallah <abdamoh1@gmail.com>
2022-08-11 14:14:49 -0700

    Include fisrt funciton that executes before main.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ *start_up- prints a string before the main function is executed
+ *
+ * Return: void
+ */
+void start_up(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
