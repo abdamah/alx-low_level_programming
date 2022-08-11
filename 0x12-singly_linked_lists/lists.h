caf9a6d7cff29818cc61311aed0858895828419b
Abdallah <abdamoh1@gmail.com>
2022-08-11 14:30:45 -0700

    Fix: start_up that executes before main.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,6 +26,6 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-void start_up(void) __attribute__ ((constructor));
+void  start_up(void) __attribute__ ((constructor));
 
 #endif
