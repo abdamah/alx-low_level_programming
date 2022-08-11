b0c777d6de27f2a565169e9aff02fd2dfe3db804
Abdallah <abdamoh1@gmail.com>
2022-08-11 06:49:35 -0700

    Fix: add_node_end fucntion .

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -17,7 +17,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		new_end_node->str = strdup(str);
 
-		if (new_end_node->str || str)
+		if (new_end_node->str || !str)
 		{
 			while (new_end_node->str[count])
 				count++;
