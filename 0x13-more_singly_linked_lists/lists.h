c993b22ae78f5fee90af214d3bc216a06212d281
Abdallah <abdamoh1@gmail.com>
2022-08-15 15:48:57 -0700

    Fix: add helper function signatues.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -37,4 +37,7 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 
+listint_t *find_listint_loop(listint_t *head);
+listint_t *is_valid(listint_t *head);
+
 #endif
