532aa581e293c12fe758719417c1212cbc24cef4
Abdallah <abdamoh1@gmail.com>
2022-08-15 13:23:36 -0700

    Fix: free_listint2 function.

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,22 +1,22 @@
 #include "lists.h"
 /**
- * free_listint2 - frees linkedList
+ * free_listint2 - frees listint_t
  * @head: double pointer to the head of the list
  * Return: void
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
+	listint_t *current;
 
 	if (head)
 	{
-
+		current = *head;
 		while (current)
 		{
 			current = current->next;
 			free(*head);
 			*head = current;
 		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
