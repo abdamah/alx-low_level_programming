5e39f29e203ca56fd2c416988c5817f1dce760b2
Abdallah <abdamoh1@gmail.com>
2022-08-22 07:48:13 -0700

    Fix: add function signature of elf.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,13 @@
 #ifndef FILEIO_H
 #define FILEIO_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -14,4 +15,16 @@ int append_text_to_file(const char *filename, char *text_content);
 
 char *create_buffer(char *file);
 void close_file(int fd);
+
+void check_elf(unsigned char *e_ident);
+	void print_magic(unsigned char *e_ident);
+	void print_class(unsigned char *e_ident);
+	void print_data(unsigned char *e_ident);
+	void print_version(unsigned char *e_ident);
+	void print_abi(unsigned char *e_ident);
+	void print_osabi(unsigned char *e_ident);
+	void print_type(unsigned int e_type, unsigned char *e_ident);
+	void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+	void close_elf(int elf);
 #endif /*FILEIO_H*/
+
