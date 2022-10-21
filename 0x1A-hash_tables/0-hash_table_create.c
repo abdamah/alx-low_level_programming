9f48afed67adad0a82c277ed2355468e3bc4cef6
Abdallah <abdamoh1@gmail.com>
2022-10-21 12:18:29 -0700

    Fix: size.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,18 +10,23 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-	unsigned long int i;
+
+	if (size < 1)
+		return (NULL);
 
 	ht = malloc(sizeof(hash_table_t));
+
 	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
+	ht->array = calloc(size, sizeof(hash_node_t *));
+
 	if (ht->array == NULL)
+	{
+		free(ht);
 		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+	}
 
 	return (ht);
 }
