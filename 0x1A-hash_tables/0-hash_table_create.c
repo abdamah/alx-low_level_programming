661bef86eb672eba1c5b3361e82a24e15e66f590
Abdallah <abdamoh1@gmail.com>
2022-10-21 11:10:15 -0700

    Fix: return type.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,24 +4,24 @@
  * hash_table_create - Creates a hash table.
  * @size: The size of the array.
  *
- * Return: on success return newly created pointer address
- *	   on failure returns NULL.
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_t;
+	hash_table_t *ht;
 	unsigned long int i;
 
-	hash_t = malloc(sizeof(hash_table_t));
-	if (hash_t == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	hash_t->size = size;
-	hash_t->array = malloc(sizeof(hash_node_t *) * size);
-	if (hash_t->array == NULL)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		hash_t->array[i] = NULL;
+		ht->array[i] = NULL;
 
-	return (hash_t);
+	return (ht);
 }
